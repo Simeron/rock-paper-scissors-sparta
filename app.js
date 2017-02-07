8e2f9cd9afdef1c6e604f6e864ef8753ce1e5045
Simeron Taak <simeron03@icloud.com>
2017-02-07 11:28:31 +0000

    files converted to node app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes/routes");
+const app = express();
+
+// view engine
+app.set("view engine", "ejs");
+
+//Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(routes);
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server listening on port ${process.env.PORT || 3000}`);
+});
